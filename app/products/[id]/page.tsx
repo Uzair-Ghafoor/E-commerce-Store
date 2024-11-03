ad45c94eb3865928131c31a9e6f03ee4287649ff
Uzair-Ghafoor <uzairgbgrt@gmail.com>
2024-11-03 20:13:52 +0500

    props

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,7 +5,12 @@ import { formatCurrency } from '@/utils/format';
 import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
 import AddToCart from '@/components/single-product/AddToCart';
 import ProductRating from '@/components/single-product/ProductRating';
-async function SingleProductPage({ params }: { params: { id: string } }) {
+
+interface Params {
+  params: { id: string };
+}
+
+async function SingleProductPage({ params }: Params) {
   const product = await fetchSingleProduct(params.id);
   const { name, image, company, description, price } = product;
   const dollarsAmount = formatCurrency(price);
